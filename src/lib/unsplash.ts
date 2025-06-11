d3ee79fb07920fefc0866b707433409b4df3fee5
svnmin <svnmindsn@gmail.com>
2025-06-11 16:48:07 +0900

    unsplash api fix

---
+++ src/lib/unsplash.ts
@@ -0,0 +1,13 @@
+import { UnsplashImage } from "@/types/types";
+
+export async function getRandomImage(): Promise<UnsplashImage | null> {
+  try{
+    const res = await fetch('/api/unsplash', { cache: 'no-store' });
+    if(!res.ok) return null;
+    const data = await res.json();
+    return data;
+  }catch(error){
+    console.error("Failed to load random image:", error);
+    return null;
+  }
+}
